1a3845c0c88ace2cbcd4cf8273819e0c1fb5bd77
Ilie1985 <ilie.marian32@yahoo.com>
2023-03-30 15:36:34 +0100

    implement sticky header

--- components/Header.js
+++ components/Header.js
@@ -5,45 +5,47 @@ import { HomeIcon } from "@heroicons/react/solid";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-      {/* Left */}
-      <div className=" cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png"
-          layout="fill"
-          className="object-contain"
-        />
-      </div>
-
-      <div className=" cursor-pointer h-24 w-10 relative lg:hidden">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
-          layout="fill"
-          className="object-contain"
-        />
-      </div>
-
-      {/* Middle */}
-      <div className="relative mt-1">
-        <div className="absolute top-2 left-2">
-          <SearchIcon className="h-5 text-gray-500 " />
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        {/* Left */}
+        <div className=" cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png"
+            layout="fill"
+            className="object-contain"
+          />
         </div>
-        <input
-          type="text"
-          placeholder="Search"
-          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
-        />
-      </div>
 
-      {/* Right */}
-      <div className="flex space-x-4 items-center">
-        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <img
-          src="https://media.licdn.com/dms/image/C4D03AQHlMq3vFRy6DQ/profile-displayphoto-shrink_800_800/0/1645124997754?e=2147483647&v=beta&t=lCWYwD_G_je1XkeucVLOEDFyBTUprs0V46AFOEJUDo0"
-          alt="marian-pic"
-          className="h-10 rounded-full cursor-pointer"
-        />
+        <div className=" cursor-pointer h-24 w-10 relative lg:hidden">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
+            layout="fill"
+            className="object-contain"
+          />
+        </div>
+
+        {/* Middle */}
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <SearchIcon className="h-5 text-gray-500 " />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+
+        {/* Right */}
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <img
+            src="https://media.licdn.com/dms/image/C4D03AQHlMq3vFRy6DQ/profile-displayphoto-shrink_800_800/0/1645124997754?e=2147483647&v=beta&t=lCWYwD_G_je1XkeucVLOEDFyBTUprs0V46AFOEJUDo0"
+            alt="marian-pic"
+            className="h-10 rounded-full cursor-pointer"
+          />
+        </div>
       </div>
     </div>
   );
