e8b5c8b8a2c9d805b9d8ff33b16ed60a95abe3ed
Ilie1985 <ilie.marian32@yahoo.com>
2023-03-30 16:54:41 +0100

    create header and image section of post comp

--- components/Post.js
+++ components/Post.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import {
+  DotsHorizontalIcon,
+  HeartIcon,
+  ChatIcon,
+  BookmarkIcon,
+  EmojiHappyIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 
-const Post = ({post}) => {
+const Post = ({ post }) => {
   return (
-    <div>
-      <h1>{post.username}</h1>
+    <div className="bg-white my-7 border rounded-md">
+      {/* Post Header */}
+      <div className="flex items-center p-5">
+        <img
+          src={post.userImg}
+          alt={post.username}
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+        />
+        <p className="font-bold flex-1">{post.username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+      {/* Post Image */}
+      <img src={post.img} alt={post.img} className="object-cover w-full"/>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
--- components/Story.js
+++ components/Story.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const Story = ({user}) => {
+const Story = ({ user }) => {
   return (
     <div>
-      <img src={user.img} alt={user.username} className="h-14 rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out"/>
-      <p className='text-xs w-14 truncate'>{user.username}</p>
+      <img
+        src={user.img}
+        alt={user.username}
+        className="h-14 rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out"
+      />
+      <p className="text-xs w-14 truncate">{user.username}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Story
+export default Story;
