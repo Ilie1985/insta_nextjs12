9f671ae230dadc10887910d5dabaca594fd9f25a
Ilie1985 <ilie.marian32@yahoo.com>
2023-03-30 15:32:06 +0100

    add menu and profile image right of the header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/outline";
+import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between max-w-6xl">
+    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
       {/* Left */}
       <div className=" cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
         <Image
@@ -27,11 +28,23 @@ const Header = () => {
         <div className="absolute top-2 left-2">
           <SearchIcon className="h-5 text-gray-500 " />
         </div>
-        <input type="text"  placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"/>
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+        />
       </div>
 
       {/* Right */}
-      <h1>Right side</h1>
+      <div className="flex space-x-4 items-center">
+        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <img
+          src="https://media.licdn.com/dms/image/C4D03AQHlMq3vFRy6DQ/profile-displayphoto-shrink_800_800/0/1645124997754?e=2147483647&v=beta&t=lCWYwD_G_je1XkeucVLOEDFyBTUprs0V46AFOEJUDo0"
+          alt="marian-pic"
+          className="h-10 rounded-full cursor-pointer"
+        />
+      </div>
     </div>
   );
 };
