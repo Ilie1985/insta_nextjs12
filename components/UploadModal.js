9967b40d84a56e9409b5f9f0276477d0b95d512a
Ilie1985 <ilie.marian32@yahoo.com>
2023-04-03 12:30:27 +0100

    complete the upload modal comp

--- components/UploadModal.js
+++ components/UploadModal.js
@@ -1,23 +1,71 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import { modalState } from "../atom/modalAtom";
 import Modal from "react-modal";
+import { CameraIcon } from "@heroicons/react/outline";
 
 const UploadModal = () => {
   const [open, setOpen] = useRecoilState(modalState);
+  const filePickerRef = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null);
 
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result);
+    };
+  };
   return (
     <div>
       {open && (
         <Modal
-          className="max-w-lg w-[90%] h-[300px] absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow:md"
+          className="max-w-lg w-[90%] p-6 h-[300px] absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow:md"
           isOpen={open}
           onRequestClose={() => {
             setOpen(false);
+            setSelectedFile(null);
           }}
         >
           <div className="flex flex-col justify-center items-center h-[100%]">
-            <h1>Modal</h1>
+            {selectedFile ? (
+              <img
+                className="w-full max-h-[250px] object-cover cursor-pointer"
+                src={selectedFile}
+                alt=""
+                onClick={() => {
+                  setSelectedFile(null);
+                }}
+              />
+            ) : (
+              <CameraIcon
+                className="cursor-pointer h-14 bg-red-200 p-2 rounded-full border-2 text-red-500"
+                onClick={() => {
+                  filePickerRef.current.click();
+                }}
+              />
+            )}
+
+            <input
+              type="file"
+              hidden
+              ref={filePickerRef}
+              onChange={addImageToPost}
+            />
+            <input
+              type="text"
+              maxLength="150"
+              placeholder="Please enter your caption"
+              className="m-4 border-none text-center w-full focus:ring-0 "
+            />
+            <button
+              className="w-full bg-red-600 text-white p-2 shadow-md hover:brightness-125 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100"
+              disabled
+            >
+              Upload Post
+            </button>
           </div>
         </Modal>
       )}
