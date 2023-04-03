a5659a21bea6b3eb008be20488363b106de717f0
Ilie1985 <ilie.marian32@yahoo.com>
2023-04-03 15:49:06 +0100

    get post datafrom database show it in Feed section

--- components/Posts.js
+++ components/Posts.js
@@ -1,30 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Posts = () => {
-  const posts = [
-    {
-      id: "1",
-      username: "marianilie",
-      userImg:
-        "https://media.licdn.com/dms/image/C4D03AQHlMq3vFRy6DQ/profile-displayphoto-shrink_800_800/0/1645124997754?e=2147483647&v=beta&t=lCWYwD_G_je1XkeucVLOEDFyBTUprs0V46AFOEJUDo0",
-      img: "https://images.unsplash.com/photo-1680034200933-09075ddb0843?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2MXx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
-      caption: "Into the wild",
-    },
-    {
-      id: "2",
-      username: "mario12",
-      userImg:
-        "https://media.licdn.com/dms/image/C4D03AQHlMq3vFRy6DQ/profile-displayphoto-shrink_800_800/0/1645124997754?e=2147483647&v=beta&t=lCWYwD_G_je1XkeucVLOEDFyBTUprs0V46AFOEJUDo0",
-      img: "https://images.unsplash.com/photo-1679872995990-a9811773f3d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMzV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
-      caption: "Check them out !",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+    return unsubscribe;
+  }, []);
   return (
     <div>
       {posts.map((post) => {
-        return <Post key={post.id} post={post} />;
+        return (
+          <Post
+            key={post.id}
+            id={post.id}
+            username={post.data().username}
+            userImg={post.data().profileImg}
+            img={post.data().image}
+            caption={post.data().caption}
+          />
+        );
       })}
     </div>
   );
